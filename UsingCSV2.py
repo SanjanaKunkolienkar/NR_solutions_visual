058eef887d2259933bf0e46db63bffffb3a12f49
SanjanaKunkolienkar <99448403+SanjanaKunkolienkar@users.noreply.github.com>
2025-06-25 17:46:20 -0500

    Few tweaks

--- UsingCSV2.py
+++ UsingCSV2.py
@@ -5,7 +5,7 @@ from matplotlib.colors import LinearSegmentedColormap
 import os
 
 cwd = os.getcwd()
-path = os.path.join(cwd, 'Inputs\\New_PW_solutions_2bus.csv')
+path = os.path.join(cwd, 'Inputs\\New_PW_solutions_2bus_Polar.csv')
 
 # --------------------------
 # Load CSV
@@ -24,14 +24,24 @@ max_iter = np.max(np.abs(Z))
 
 # Define colors: from negative to positive
 # Order: black -> dark yellow -> light yellow -> white -> light red -> dark red -> black
+# cmap_colors = [
+#     #(0.0, 'black'),      # start (negative extreme)
+#     (0.0, '#CCCC00'),   # dark yellow
+#     (0.45, '#FFFF99'),   # light yellow
+#     (0.5, 'white'),      # midpoint (zero)
+#     (0.55, '#FF9999'),   # light red
+#     (1.0, '#990000'),   # dark red
+#     #(1.0, 'black')       # end (positive extreme)
+# ]
+
 cmap_colors = [
-    (0.0, 'black'),      # start (negative extreme)
-    (0.25, '#CCCC00'),   # dark yellow
-    (0.45, '#FFFF99'),   # light yellow
-    (0.5, 'white'),      # midpoint (zero)
-    (0.55, '#FF9999'),   # light red
-    (0.75, '#990000'),   # dark red
-    (1.0, 'black')       # end (positive extreme)
+    (0.0, '#4B0082'),  # deep purple (negative extreme)
+    (0.2, '#6A4C93'),
+    (0.35, '#9F86C0'),
+    (0.5, '#E0D4A3'),  # beige/light at zero
+    (0.65, '#F6AE2D'),
+    (0.8, '#F26419'),
+    (1.0, '#B23A00')   # dark brown/orange (positive extreme)
 ]
 
 custom_cmap = LinearSegmentedColormap.from_list('yellow_red_black', cmap_colors)
@@ -53,11 +63,12 @@ cb = fig.colorbar(c, ax=ax, label='Iterations')
 
 ax.set_xlabel('V angle [deg]', fontsize=20)
 ax.set_ylabel('V mag [pu]', fontsize=20)
-ax.set_title('NR ROC', fontsize=20)
+# ax.set_title('NR ROC', fontsize=20)
 ax.tick_params(axis='x', labelsize=20)
 ax.tick_params(axis='y', labelsize=20)
 
 plt.tight_layout()
+plt.savefig('NR_ROC_2bus.png', dpi=600, bbox_inches='tight')
 plt.show()
 
 
